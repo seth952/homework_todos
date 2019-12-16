b375ba1798ad1c57b3b944de3916059602c36023
User <user@users-mbp.home>
2019-12-16 22:23:46 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,24 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: '#app'
-
+    el: "#app",
+    data: {
+      tasks: [
+        {name: "Christmas Shopping", complete: false},
+        {name: "Divorce the Mrs", complete: false},
+        {name: "Eat an entire Pig", complete: true}
+      ],
+      newTask: ""
+    },
+    methods: {
+      saveNewTask: function(){
+        let taskObject = {name: this.newTask, complete: false}
+        this.tasks.push(taskObject)
+        this.newTask = ""
+      },
+      taskComplete: function(index){
+        this.tasks[index].complete = true
+      }
+    }
   });
-})
+});
